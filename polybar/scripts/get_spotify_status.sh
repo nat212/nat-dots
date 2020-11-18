cb5ef361890d5ea93bd809528d20f6d88b9ed86b
Natasha Draper <natasha@draper.net.za>
2020-11-18 09:53:58 +0200

    Some changes to player status and add gnome-pomodoro

--- polybar/scripts/get_spotify_status.sh
+++ polybar/scripts/get_spotify_status.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-if ! playerctl --player=spotify status >/dev/null 2>&1; then
+if ! playerctl --ignore-player=chromium status >/dev/null 2>&1; then
   echo "No music is playing"
   polybar-msg -p "$(pgrep -f "polybar main")" hook spotify-play-pause 3 1>/dev/null 2>&1
   exit 0;
 fi
 
-if [ "$(playerctl --player=spotify status)" = "Stopped" ]; then
+if [ "$(playerctl --ignore-player=chromium status)" = "Stopped" ]; then
     echo "No music is playing"
     polybar-msg -p "$(pgrep -f "polybar main")" hook spotify-play-pause 3 1>/dev/null 2>&1
-elif [ "$(playerctl --player=spotify status)" = "Paused"  ]; then
+elif [ "$(playerctl --ignore-player=chromium status)" = "Paused"  ]; then
     polybar-msg -p "$(pgrep -f "polybar main")" hook spotify-play-pause 2 1>/dev/null 2>&1
-    playerctl --player=spotify metadata --format "{{ title }} - {{ artist }}"
+    playerctl --ignore-player=chromium metadata --format "{{ title }} - {{ artist }}"
 else # Can be configured to output differently when player is paused
     polybar-msg -p "$(pgrep -f "polybar main")" hook spotify-play-pause 1 1>/dev/null 2>&1
-    playerctl --player=spotify metadata --format "{{ title }} - {{ artist }}"
+    playerctl --ignore-player=chromium metadata --format "{{ title }} - {{ artist }}"
 fi
 
 exit 0
